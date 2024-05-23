22663c61a37539dc7d64724676c6ec5710bbcb45
josedavidp12 <perafandavid322@gmail.com>
2024-05-23 14:17:18 -0500

    Decametros

--- app/src/main/java/com/kevin/examen/MainActivity.kt
+++ app/src/main/java/com/kevin/examen/MainActivity.kt
@@ -5,9 +5,7 @@ import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.kevin.examen.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
